53717032f172eb996385e4395bf0807017f3b8e9
sensible-shell-8529 <diana.lee116@gmail.com>
2019-01-30 19:43:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,7 +12,6 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def speak_to_grandma(phrase) 
-  if #{phrase}== #{phrase}.downcase
+if speak_to_grandma(phrase) == speak_to_grandma(phrase).downcase
   return "HUH?! SPEAK UP, SONNY!"
 end
