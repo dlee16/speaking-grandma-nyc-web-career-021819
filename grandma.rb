b9c36814fc1ea0158b605a4ac8e485c9c600ef92
sensible-shell-8529 <diana.lee116@gmail.com>
2019-01-30 19:36:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,4 @@
 def speak_to_grandma(phrase) 
   if #{phrase}== #{phrase}.downcase
   return "HUH?! SPEAK UP, SONNY!"
-  elsif #{phrase}== #{phrase}.upcase
- return "NO, NOT SINCE 1938!"
-else 
-  return "I LOVE YOU TOO PUMPKIN!"
-end
 end
