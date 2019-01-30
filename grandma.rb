5832555d1c26ebd3b6a39447022894a4bc9dae69
sensible-shell-8529 <diana.lee116@gmail.com>
2019-01-30 19:57:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,11 +13,11 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase) 
-  if (phrase=="#{phrase}".downcase)
-  return "HUH?! SPEAK UP, SONNY!"
-  elsif (phrase =="#{phrase}".upcase)
-  return "NO, NOT SINCE 1938!"
-else (phrase == "I LOVE YOU GRANDMA!")
+  if (phrase == "I LOVE YOU GRANDMA!")
   return "I LOVE YOU TOO PUMPKIN!"
+  elsif (phrase =="#{phrase}".downcase)
+  return "HUH?! SPEAK UP, SONNY!"
+else 
+  return "NO, NOT SINCE 1938!"
 end
 end
